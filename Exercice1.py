c5707847cbe033312ebb7505f73341cf2b2c86d5
Ross260 <rossketcha@gmail.com>
2025-02-26 00:37:54 +0100

    csv file

--- Exercice1.py
+++ Exercice1.py
@@ -15,6 +15,7 @@ Bonus:
 
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://lemonde.fr/"  # Site de livres à scraper
 response = requests.get(url)  # Envoie une requête GET
@@ -34,17 +35,30 @@ if response.status_code == 200:
     # car d'après mon analyse c'est eux qui contiennent les titres des articles
     titles = soup.find_all('p', class_='article__title')
 
-    # Question bonus
+    # QUESTION BONUS
     # Trouver toutes les balises <a> avec la classe "article article--list" d'après mon analyse sur le site
     links = soup.find_all('a', class_='article')
 
     i = 0
     # Je parcours tous les titres recupérés, les balises <a> trouvées et j'extrais les href
     # j'utilise le zip() ici pour combiner les listes et les parcourir
-    for title, link in zip(titles, links):
+    for title, link in zip(titles[:20], links[:20]):
         i = i + 1
         href = link.get('href')
         print(f"{i}- {title.string}  {href}")
 
+    # Création du fichier CSV en mode écriture
+    with open("Scraping.csv", mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+
+        # Écrire l'en-tête
+        writer.writerow(["Numéro", "Titre", "Lien"])
+
+        # Insérer les données extraites
+        for i, (title, link) in enumerate(zip(titles[:20], links[:20]), start=1):
+            href = link.get('href')
+            writer.writerow([i, title.string, href])
+
+    print("Les résultats ont été enregistrés dans le fichier csv avec succès")
 else:
     print(f"Erreur {response.status_code}")
