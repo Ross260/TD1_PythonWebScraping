0b73464c5b52dd9db8e228f6f1dc0536448be6dc
Ross260 <rossketcha@gmail.com>
2025-03-01 02:26:46 +0100

    Elements sur les produits collecter, analyser, transformer et structurer au format csv et Excel

---
+++ Exercice 4/Exercice4.py
@@ -0,0 +1,15 @@
+"""
+ Scraper des données météorologiques
+
+Objectif : Obtenir et afficher la météo d’une ville en temps réel.
+
+Consigne :
+1. Trouvez un site météo qui affiche les températures par ville (ex : Météo France).
+2. Récupérez la température actuelle pour une ville donnée.
+3. Affichez-la sous forme d’un message : "La température à Paris est de 15°C."
+
+Bonus :
+-> Ajoutez la météo des 5 prochains jours.
+-> Stockez les données en JSON.
+
+"""
